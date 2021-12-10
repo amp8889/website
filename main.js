7374f4c1709cc755bcfc7623b95f75491593696b
Aidan Pavlik <apavlik@hawk.iit.edu>
2021-12-09 17:04:31 -0800

    Create JS that saves a users imput to variable

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
+import FileSaver.js
+
 document.querySelector('html').className = 'js';
 
 
 var p = document.querySelector('p');
+var bt = document.getElementById('bt');
+var input = document.getElementById("textholder").value;
+var ph = document.getElementById('ph');
+var comebackText = "I get what you are saying, but, what if you are wrong? Thinks to think about."
 
-p.addEventListener('click', function(){
+bt.addEventListener('click', function(){
+
+  var input = document.getElementById("textholder").value;
+  ph = comebackText;
+  document.getElementById(ph).text(comebackText);
+
 
   console.log('PLEASE WORK')
+  console.log(input);
+
+
 });
+
 
 
 
